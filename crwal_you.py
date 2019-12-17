038da53e539e98168cc22ea84fad618e5f5bc093
SoonMinKwun <tnsals9603@naver.com>
2019-12-17 20:03:36 +0900

    ex02 ㅎㅎ

--- crwal_you.py
+++ crwal_you.py
@@ -4,6 +4,7 @@ import time
 import urllib.request #
 from selenium.webdriver import Chrome
 import re
+import  pandas as pd
 
 
 from selenium.webdriver.chrome.options import Options
@@ -35,7 +36,7 @@ like = info1.find('div',{'id':'top-level-buttons'}).find_all('yt-formatted-strin
 unlike = info1.find('div',{'id':'top-level-buttons'}).find_all('yt-formatted-string')[1].text #싫어요수
 date = soup.find('span',{'class':'date style-scope ytd-video-secondary-info-renderer'}).text#영상업로드날
 youtube_info={"title":title, "view":view,"like":like,"unlike":unlike,"date":date}
-
+asd=soup.find_all('div',{'id':'header-author'},{'class':'style-scope ytd-comment-renderer'})
 print("youtube_info")
 print(youtube_info)
 #print(comment1)
@@ -46,10 +47,18 @@ print(youtube_info)
 #print(unlike)
 #print(date)
 cmtlist=[]
+cmt_ll=[]
 count=0
+for it in asd:
+    it.find_all({'id':'author-text'})
 for items in soup:
-    div=items.find_all('yt-formatted-string',attrs={'id':'content-text'})
-    #print(div)
+    div=items.find_all('yt-formatted-string',attrs={'id':'content-text'})  #comment
+    div2=items.find_all('span',attrs={'class':'style-scope ytd-comment-renderer'})
+    for list2 in div2:
+        if (list2.string != None) and (list2.string != 'VIEW'):
+            ccc=(list2.string.replace('\n',"")).strip()
+            if(ccc != '•') and (ccc != ''):
+                cmt_ll.append([ccc])
     for lists in div:
         if lists != None:
             try:
@@ -60,6 +69,12 @@ for items in soup:
             else:
                 pass
 
-for t in cmtlist:
+casd={"comment" : cmtlist , "comment_id" : cmt_ll}
+print(casd)
+for t in casd:
     print(t)
     print("----------------------------------------------------")
+
+frame=pd.DataFrame(casd)
+print(frame)
+frame.to_csv("frame.csv",mode="w",encoding="utf-8-sig")
